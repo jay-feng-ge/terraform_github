a9fa549d606ae0c34e3480f009cff6fcd8390659
jay-feng-ge <jay.feng@goexpedi.com>
2021-06-17 20:36:49 -0700

    add files

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+variable "token" {
+  type = string
+  description = "Github OAuth Token"
+}
+
+# Configure the GitHub Provider
+provider "github" {
+  token = var.token
+}
