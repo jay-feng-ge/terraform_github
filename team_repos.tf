8a02fc07466f64578e4080f7a601849c9d6f1fa5
Jay Feng <jayfeng10@berkeley.edu>
2021-07-06 12:12:30 -0700

    added another team repo for demo

--- team_repos.tf
+++ team_repos.tf
@@ -6,3 +6,12 @@ module "make_team_1_test_repo" {
   team_name = "team_1"
   repo_name = "team_1_test_repo"
 }
+
+module "make_team_1_test_repo_2" {
+  source = "./modules/team_repo"
+
+  token = var.token
+
+  team_name = "team_1"
+  repo_name = "team_1_test_repo_2"
+}
